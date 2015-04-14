9b60d926f6eb967836b3846fce19909e019a0b9f
Bogdan Veres <veresbogdan91@gmail.com>
2015-04-14 15:31:54 +0200

    Added Channel for Add a new product for a user

--- src/main/java/intwifeel/controller/ProductController.java
+++ src/main/java/intwifeel/controller/ProductController.java
@@ -1,12 +1,9 @@
 package intwifeel.controller;
 
 import intwifeel.model.ProductEntity;
-import intwifeel.model.UserEntity;
 import intwifeel.service.ProductService;
-import intwifeel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
